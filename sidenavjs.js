e98e72dab1ea06fa4c893df8a38bce143ae06919
dvoranii <dvoranii@sheridancollege.ca>
2021-09-10 15:33:49 -0400

    First commit

---
+++ sidenavjs.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main").style.color="white";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.color="black";
+    document.body.style.backgroundColor = "white";
+  }
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
